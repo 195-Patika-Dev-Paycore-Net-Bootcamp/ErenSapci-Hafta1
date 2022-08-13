f5b9f8b710e1ee687aee1a7387151af86b064f8e
Eren Şapçı <sapci.erenn@gmail.com>
2022-08-13 13:30:23 +0300

    The interest amount process has been updated
    The interest amount process has been updated and new Screenshots added.

--- BootcampCompounInterestCalculator/Controllers/CompoundInterestController.cs
+++ BootcampCompounInterestCalculator/Controllers/CompoundInterestController.cs
@@ -17,11 +17,17 @@ namespace BootcampCompoundInterest.Controllers
             CompoundInterestResponse compoundInterestResponse = new() //Created object of CompoundInterestResponse class
             {
                 //Compound interest calculation has been carried out
-                InterestAmount = compoundInterestRequest.Balance,
-                TotalBalance = compoundInterestRequest.Balance * Math.Pow((1 + compoundInterestRequest.InterestRate / 100), compoundInterestRequest.InterestTerm)
+                
+                TotalBalance = compoundInterestRequest.Balance * Math.Pow((1 + compoundInterestRequest.InterestRate / 100), compoundInterestRequest.InterestTerm),
+                
             };
+
+            //Calculation of interest amount
+            compoundInterestResponse.InterestAmount = compoundInterestResponse.TotalBalance - compoundInterestRequest.Balance;
+            
             //Response object returned
             return compoundInterestResponse;
+        
         }
 
     }
